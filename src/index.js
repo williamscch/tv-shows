9c0b7152ba38d05f7791ec978fd1e2c54244adee
wdavidcch <david.13.2121@gmail.com>
2022-07-25 18:57:20 -0400

    Add structure of the nav bar and some style

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import './style.css';
-import tvlogo from '../images/tvlogo.png';
+import tvLogo from '../images/tvlogo.png';
 
 const getShows = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   const JsonResponse = await response.json();
-  const list = document.querySelector('ul');
+  const list = document.querySelector('.shows');
   list.innerText = JsonResponse[1].name;
   list.append(JsonResponse[1].summary);
 };
 
 getShows();
 
-const body = document.querySelector('body');
-const imagelogo = document.createElement('img');
-imagelogo.src = tvlogo;
-body.appendChild(imagelogo);
+const navLogoContainer = document.querySelector('.logo');
+const imageLogo = document.createElement('img');
+imageLogo.classList.add('logo-nav');
+imageLogo.src = tvLogo;
+navLogoContainer.appendChild(imageLogo);
