cddfc7aa8a8c0e9a9404b584c633901c742359be
wdavidcch <david.13.2121@gmail.com>
2022-07-26 12:52:09 -0400

    Display shows in the screen

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import './style.css';
 import tvLogo from '../images/tvlogo.png';
+// eslint-disable-next-line import/no-cycle
+import displayScreen from './modules/display.js';
+
+const list = document.querySelector('.shows');
 
 const getShows = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   const JsonResponse = await response.json();
-  const list = document.querySelector('.shows');
-  list.innerText = JsonResponse[1].name;
-  list.append(JsonResponse[1].summary);
+  const mostRatedShows = JsonResponse.filter((show) => show.rating.average > 8.5);
+  return mostRatedShows;
 };
-
-getShows();
 
 const navLogoContainer = document.querySelector('.logo');
 const imageLogo = document.createElement('img');
 imageLogo.classList.add('logo-nav');
 imageLogo.src = tvLogo;
 navLogoContainer.appendChild(imageLogo);
+
+displayScreen();
+
+export { getShows, list };
