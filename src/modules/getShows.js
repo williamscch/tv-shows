e7454ebdaaafae3710b72cde8e10bc9e5d2b354d
diego <diegoyon@outlook.com>
2022-07-26 13:23:10 -0600

    Add comments section to popup window

--- src/modules/getShows.js
+++ src/modules/getShows.js
@@ -1,8 +1,8 @@
 const getShows = async () => {
-  const response = await fetch("https://api.tvmaze.com/shows");
+  const response = await fetch('https://api.tvmaze.com/shows');
   const JsonResponse = await response.json();
   const mostRatedShows = JsonResponse.filter(
-    (show) => show.rating.average > 8.5
+    (show) => show.rating.average > 8.5,
   );
   return mostRatedShows;
 };
