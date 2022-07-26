c94c2cc6c32bdd1b92f5b2104505afe82b423c53
diego <diegoyon@outlook.com>
2022-07-26 11:18:47 -0600

    Finish merging display and popup branches

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
-import './style.css';
-import tvLogo from '../images/tvlogo.png';
-<<<<<<< HEAD
-import displayModal from './displayModal.js';
-=======
-// eslint-disable-next-line import/no-cycle
-import displayScreen from './modules/display.js';
+import "./style.css";
+import tvLogo from "../images/tvlogo.png";
+import displayModal from "./displayModal.js";
+import displayScreen from "./modules/display.js";
 
-const list = document.querySelector('.shows');
->>>>>>> display
+const list = document.querySelector(".shows");
 
 const getShows = async () => {
-  const response = await fetch('https://api.tvmaze.com/shows');
+  const response = await fetch("https://api.tvmaze.com/shows");
   const JsonResponse = await response.json();
-  const mostRatedShows = JsonResponse.filter((show) => show.rating.average > 8.5);
+  const mostRatedShows = JsonResponse.filter(
+    (show) => show.rating.average > 8.5
+  );
   return mostRatedShows;
 };
 
-const navLogoContainer = document.querySelector('.logo');
-const imageLogo = document.createElement('img');
-imageLogo.classList.add('logo-nav');
+const navLogoContainer = document.querySelector(".logo");
+const imageLogo = document.createElement("img");
+imageLogo.classList.add("logo-nav");
 imageLogo.src = tvLogo;
 navLogoContainer.appendChild(imageLogo);
 
-<<<<<<< HEAD
 // Boton de prueba
-const btn = document.querySelector('.btn1');
-btn.addEventListener('click', displayModal);
-=======
+const btn = document.querySelector(".btn1");
+btn.addEventListener("click", displayModal);
 displayScreen();
 
 export { getShows, list };
->>>>>>> display
