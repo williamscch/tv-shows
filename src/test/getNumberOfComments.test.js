1b91f36a9618fb9b02084d908fd1cd3689bf8d04
diego <diegoyon@outlook.com>
2022-07-27 11:12:18 -0600

    Add test to comment counter

---
+++ src/test/getNumberOfComments.test.js
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import getNumberOfComments from '../modules/getNumberOfComments';
+
+describe('getNumberOfComments tests', () => {
+  test('Should count all li elements to be 3', () => {
+    document.body.innerHTML =
+      '<ul class="comments"><li></li><li></li><li></li></ul>';
+    const number = getNumberOfComments();
+    expect(number).toBe(3);
+  });
+
+  test('Should count all li elements to be 0', () => {
+    document.body.innerHTML = '<ul class="comments"></ul>';
+    const number = getNumberOfComments();
+    expect(number).toBe(0);
+  });
+
+  test('Should count all li elements to be 9', () => {
+    document.body.innerHTML =
+      '<ul class="comments"><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li></ul>';
+    const number = getNumberOfComments();
+    expect(number).toBe(9);
+  });
+});
+
